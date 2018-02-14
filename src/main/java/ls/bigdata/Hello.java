c8e39f4e8fb039068206295daa141e7413f567ec
lishuai2016 <1830473670@qq.com>
2018-02-14 10:54:21 +0800

    整合之前的各个项目，暂时没有把spark添加进去

---
+++ src/main/java/ls/bigdata/Hello.java
@@ -0,0 +1,10 @@
+package ls.bigdata;
+
+/**
+ * Created by lishuai on 2018/2/12.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("11111111");
+    }
+}
